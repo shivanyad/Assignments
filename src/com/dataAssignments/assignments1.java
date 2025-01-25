7df4d244d628aedf2e1f2a251c287706535aa30c
shivanyad <ind.shivan@gmail.com>
2025-01-25 15:29:09 +0530

    DataType

--- src/com/dataAssignments/assignments1.java
+++ src/com/dataAssignments/assignments1.java
@@ -3,7 +3,71 @@ package com.dataAssignments;
 public class assignments1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+     
+		
+		double temperature = 25.5;
+		System.out.println(temperature);
+		
+		boolean placedOrder = false;
+        System.out.println(placedOrder);
+        
+        int phoneNumber = 1234567890;
+        System.out.println(phoneNumber);
+        
+        double accountBalance = 1000.50;
+        System.out.println(accountBalance);
+        
+        String emailAddress = "john.doe@example.com";
+        System.out.println(emailAddress);
+        
+        double latitude = 37.7749;
+        System.out.println(latitude);
+        
+        double longitude = -122.4194;
+        System.out.println(longitude);
+        
+        boolean maritalStatus = true; 
+        System.out.println(maritalStatus);
+        
+        String personOccupation = "Software Engineer";
+        System.out.println(personOccupation);
+        
+        String favouriteColour = "Blue";
+        System.out.println(favouriteColour);
+        
+        int currentYear=  2023;
+        System.out.println(currentYear);
+
+        long numberOfFollowers = 1000000;  
+        System.out.println(numberOfFollowers);
+        
+        double ratingOfMovie = 7.5;
+        System.out.println(ratingOfMovie);
+        
+        char bloodType = 'A';
+        System.out.println(bloodType);
+
+        String titleOfBook= "To Kill a Mockingbird";
+        System.out.println(titleOfBook);
+
+        int companyEmployees = 500;
+        System.out.println(companyEmployees);
+	
+   	    String timeOfEvent =  "2:30 PM";
+        System.out.println(timeOfEvent);
+
+   	    String	nameOfCountry=  "United States"	;
+        System.out.println(nameOfCountry);
+
+   	    String	personEyeColor= "Brown";
+        System.out.println(personEyeColor);
+
+   	    String	personBirthPlace= "New York City";
+        System.out.println(personBirthPlace);
+
+        	
+        double distanceBetweenTwoCities = 200.5;
+        System.out.println(distanceBetweenTwoCities);
 
 	}
 
